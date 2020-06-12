71b0b76ce6acb0b62144e5b5a1a57a6c3f661be0
simple <1228115857@qq.com>
2020-06-12 17:07:24 +0800

    添加服务启动配置

--- src/Ketchup.Zero.Server/Program.cs
+++ src/Ketchup.Zero.Server/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using System.IO;
+using System.Net;
+using Autofac.Extensions.DependencyInjection;
+using Ketchup.Core.Configurations;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace Ketchup.Zero.Server
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.SetBasePath(Directory.GetCurrentDirectory());
+                    config.AddJsonFile("config/server.json", true, true);
+                }).ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder
+                        .ConfigureKestrel(options =>
+                        {
+                            var config = AppConfig.ServerOptions;
+                            options.Listen(new IPEndPoint(IPAddress.Parse(config.Ip), config.Port),
+                                listenOptions => { listenOptions.Protocols = HttpProtocols.Http2; });
+                        })
+                        .UseStartup<Startup>();
+                });
         }
     }
 }
---
+++ src/Ketchup.Zero.Server/Startup.cs
@@ -0,0 +1,69 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Ketchup.Core;
+using Ketchup.Core.Configurations;
+using Ketchup.Core.Utilities;
+using Ketchup.Grpc.Internal.Intercept;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Ketchup.Zero.Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            AppConfig.Configuration = (IConfigurationRoot)configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add things to the service collection.
+            services.AddGrpc(grpc => grpc.Interceptors.Add<HystrixCommandIntercept>());
+
+        }
+
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            // Add things to the service collection that are only for the
+            // development environment.
+            services.AddGrpc(grpc => grpc.Interceptors.Add<HystrixCommandIntercept>());
+            //services.AddGrpc();
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            // Add things to the Autofac ContainerBuilder.
+            builder.AddCoreService().AddEventBusService().RegisterModules();
+        }
+
+        public void ConfigureProductionContainer(ContainerBuilder builder)
+        {
+            // Add things to the ContainerBuilder that are only for the
+            // production environment.
+            builder.AddCoreService().RegisterModules();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // Set up the application for development.
+
+            ServiceLocator.Current = app.ApplicationServices.GetAutofacRoot();
+
+            app.UseRouting();
+            app.UseKetchup();
+        }
+
+        public void ConfigureStaging(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            // Set up the application for staging.
+            ServiceLocator.Current = app.ApplicationServices.GetAutofacRoot();
+            app.UseRouting();
+
+            app.UseKetchup();
+
+        }
+    }
+}
