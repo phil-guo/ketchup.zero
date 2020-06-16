81f42259441fa0b95880f1ad205e349a5e67f861
simple <1228115857@qq.com>
2020-06-16 16:41:39 +0800

    automapper 映射忽视null

--- src/Ketchup.Zero.Application/PermissionProfile.cs
+++ src/Ketchup.Zero.Application/PermissionProfile.cs
@@ -11,11 +11,15 @@ namespace Ketchup.Zero.Application
             CreateMap<SysMenu, MenuDto>()
                 .ForMember(destination => destination.Icon, opt => opt.NullSubstitute(""));
 
-            CreateMap<SysRole, RoleDto>();
+            CreateMap<SysRole, RoleDto>()
+                .ForMember(destination => destination.Name, opt => opt.NullSubstitute(""))
+                .ForMember(destination => destination.Remark, opt => opt.NullSubstitute(""));
             CreateMap<SysOperate, OperateDto>()
                 .ForMember(destination => destination.Name, opt => opt.NullSubstitute(""))
                 .ForMember(destination => destination.Remark, opt => opt.NullSubstitute(""));
-
+            CreateMap<SysUser, SysUserDto>()
+                .ForMember(destination => destination.Password, opt => opt.NullSubstitute(""))
+                .ForMember(destination => destination.UserName, opt => opt.NullSubstitute(""));
         }
     }
 }
