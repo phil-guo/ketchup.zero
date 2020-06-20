3e69fdb902637fe21c04494b3a5ff6e460e2ba07
simple <1228115857@qq.com>
2020-06-20 22:04:45 +0800

    zero permission 组件

--- backstage/src/views/permissions/zero.component/index.vue
+++ backstage/src/views/permissions/zero.component/index.vue
@@ -1,45 +1,73 @@
 <template>
   <div>
-    <el-button
-      v-if="isSearch"
-      style="margin-right:-5px"
-      v-show="isSearch"
-      size="small"
-      @click="search"
-    >查询</el-button>
+    <el-button v-if="isSearch" style="margin-right:-5px" v-show="isSearch" size="small" @click="search">查询</el-button>
+    <el-button v-if="isInsert" style="margin-right:-5px" v-show="isInsert" size="small" @click="insert">添加</el-button>
+    <el-button v-if="isEdit" style="margin-right:-5px" v-show="isEdit" size="small" @click="edit">编辑</el-button>
+    <el-button v-if="isDelete" style="margin-right:-5px" v-show="isDelete" size="small" @click="remove">删除</el-button>
   </div>
 </template>
 <script>
-import util from "@/libs/util.js";
-import $ from "jquery";
-export default {
-  data() {
-    return {
-      permissions: [],
-      isSearch: false
-    };
-  },
-  mounted() {},
-  methods: {
-    search() {
-      this.$emit("zero-Search");
-    },
-    getMenuOperate() {
-      let vm = this;
-      var params = {
-        model: {
-          roleId: parseInt(util.cookies.get(util.globalSetting.roleId)),
-          menuId: parseInt(vm.$route.query.id)
-        }
+  import util from "@/libs/util.js";
+  import $ from "jquery";
+  export default {
+    data() {
+      return {
+        permissions: [],
+        isInsert: false,
+        isEdit: false,
+        isSearch: false,
+        isDelete: false
       };
+    },
+    mounted() {},
+    methods: {
+      search() {
+        this.$emit("zero-search");
+      },
+      insert() {
+        this.$emit("zero-insert");
+      },
 
-      util.http.post(util.requestUrl.getMenuOfOperate, params, vm, function(
-        response
-      ) {
-        vm.permissions = response.data.result;
-        vm.showButton();
-      });
+      edit() {
+        this.$emit("zero-edit");
+      },
+
+      remove() {
+        this.$emit("zero-remove");
+      },
+
+      getMenuOperate() {
+        let vm = this;
+        var params = {
+          model: {
+            roleId: parseInt(util.cookies.get(util.globalSetting.roleId)),
+            menuId: parseInt(vm.$route.query.id)
+          }
+        };
+
+        util.http.post(util.requestUrl.getMenuOfOperate, params, vm, function (
+          response
+        ) {
+          vm.permissions = response.data.result;
+          vm.showPermission();
+        });
+      },
+
+      showPermission() {
+        let vm = this;
+        $.each(vm.permissions, (key, item) => {
+          if (item == "10001") {
+            vm.isInsert = true;
+          } else if (item == "10002") {
+            vm.isEdit = true;
+          } else if (item == "10003") {
+            vm.isSearch = true;
+          } else if (item == "10004") {
+            vm.isDelete = true;
+          }
+        });
+      }
     }
-  }
-};
+  };
+
 </script>
